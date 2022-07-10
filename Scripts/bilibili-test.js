b1ea4fa897a16c7e5d83dc61e95e55053ff32e2c
zsbai <62942942+zsbai@users.noreply.github.com>
2022-07-10 14:08:44 +0800

    Create bilibili-test.js

---
+++ Scripts/bilibili-test.js
@@ -0,0 +1,24 @@
+const url = $request.url;
+let body = JSON.parse($response.body);
+
+try {
+    if (url.indexOf('feed\/index') != -1) {
+        let i = body.data.items.length;
+        while (i--) {
+            if (body.data.items[i].card_goto.indexOf('banner') != -1 ) {
+                body.data.items.splice(i,1);
+            } else if (body.data.items[i].title.indexOf('C') == -1 ||
+            body.data.items[i].title.indexOf('由') == -1) {
+                body.data.items.splice(i,1);
+
+            }
+        }
+
+    }
+} catch (e) {
+    console.log("bilibili_test , " + e)
+}
+body = JSON.stringify(body)
+$done({
+    body
+})
