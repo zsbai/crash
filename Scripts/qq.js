79599bc8d9ef7c82dff62c83246de96e3888df75
zsbai <62942942+zsbai@users.noreply.github.com>
2022-07-10 16:30:14 +0800

    Update qq.js

--- Scripts/qq.js
+++ Scripts/qq.js
@@ -1,9 +1,10 @@
 const url = $request.url;
+let body = $response.body
+
 let newJavaScript = '<script type="text/javascript" async="async" src="https://raw.githubusercontent.com/zsbai/crash/master/Scripts/anti_qq.js"></script>'
 
 var rBody = '</body>'
 try {
-  let body = $response.body
      .replace(rBody, newJavaScript);
 } catch (e) {
 concole.log(e)
